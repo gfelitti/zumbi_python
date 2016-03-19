320f5dba36b923b8827bb1762c278263639e9a2a
guilherme felitti <gfelitti@gmail.com>
2016-03-18 22:19:11 -0300

    script para contar palavras no depoimento

---
+++ lula_contagem_palavras.py
@@ -0,0 +1,23 @@
+#coding=UTF-8
+arq = open("lula.rtf")
+texto = arq.read()
+texto = texto.lower()
+arq.close()
+import string
+
+for c in ",./?:;,\/[]{}":
+    texto = texto.replace(c, " ")
+
+texto = texto.split()
+
+dic ={ }
+for p in texto:
+    if p not in dic:
+        dic[p] = 1
+    else:
+        dic[p] += 1
+
+#print(dic)
+print(dic["nao"])
+print(dic["sim"])
+#print("Alice aparece %s vezes" %dic["alice"])
